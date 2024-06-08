fffe3acb731b54cba0df18d36fad51105500ee82
walserwater <meret.walser@bluewin.ch>
2024-06-08 11:48:21 +0200

    use wrapper for CORS

--- api/getWeather.js
+++ api/getWeather.js
@@ -1,27 +1,35 @@
 const axios = require('axios');
 
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
 
-  const { city } = req.query;
-  const apiKey = process.env.API_KEY;
-
-  if (!city) {
-    return res.status(400).json({ error: 'City is required' });
-  }
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-  try {
-    const response = await axios.get(url);
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-  }
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    return await fn(req, res);
 };
+
+
+const callWeatherApi = async (req, res) => {
+
+    const { city } = req.query;
+    const apiKey = process.env.API_KEY;
+  
+    if (!city) {
+      return res.status(400).json({ error: 'City is required' });
+    }
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  
+    try {
+      const response = await axios.get(url);
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: error.toString() });
+    }
+};
+
+module.exports = allowCors(callWeatherApi);
