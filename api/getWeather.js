0a5a3f4ef9301f019eb08e2ae934a6bceecfed25
walserwater <meret.walser@bluewin.ch>
2024-06-08 11:41:26 +0200

    allow CORS directly in axios

--- api/getWeather.js
+++ api/getWeather.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
   const { city } = req.query;
   const apiKey = process.env.API_KEY;
 
